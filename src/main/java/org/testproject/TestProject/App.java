a15e185adee1f0cef35d172e0148751afab55118
airetOK <61311414+airetOK@users.noreply.github.com>
2020-09-07 22:47:19 +0300

    Update App.java

--- src/main/java/org/testproject/TestProject/App.java
+++ src/main/java/org/testproject/TestProject/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello from local repo!" );
+        System.out.println( "Hello from global repo!" );
     }
 }
