8592ac8936724502831c467ae5199e7c78ca521f
airetOK <sergey.bezrukavyy@gmail.com>
2020-09-08 22:41:25 +0300

    New commit

--- src/main/java/org/testproject/TestProject/User.java
+++ src/main/java/org/testproject/TestProject/User.java
@@ -7,23 +7,11 @@ public class User {
   private String email;
   private String password;
   private String address;
-  private String dateOfBirth;
-  
-  
-  
   
   
   
   
 	
-public String getDateOfBirth() {
-	return dateOfBirth;
-}
-
-public void setDateOfBirth(String dateOfBirth) {
-	this.dateOfBirth = dateOfBirth;
-}
-
 public String getAddress() {
 	return address;
 }
